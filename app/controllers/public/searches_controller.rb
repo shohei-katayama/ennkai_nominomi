0e6283c262475d977382657566f7ea6f2c1d97a5
nyoonn3902 <shohe.k0000@gmail.com>
2024-01-18 11:38:44 +0000

    検索機能

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -3,6 +3,7 @@ class Public::SearchesController < ApplicationController
   def search
     @content = params[:content]
     @method = params[:method]
+    @stores = Store.search_for(params[:content], params[:word])
   end
   
 end
